bbde0da6bdbba03fc47a9cf0b890ce5b16ffb944
Mwaniki83 <munyimorris83@gmail.com>
2023-08-17 06:38:32 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,12 @@
 
 /**
  * print_most_numbers - checks for a digit (0 through 9).
- *
  * Return: Always 0.
  */
 
 void print_most_numbers(void)
 {
-	int c;
+	int x;
 
 	for (x = 48; x < 58; x++)
 	{
